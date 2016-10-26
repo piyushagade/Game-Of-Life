d8efb0bfa150785cb9a52c640bdb6b0375faf930
piyushagade <piyushagade@gmail.com>
2016-10-26 11:42:07 -0400

    Fixed neighbours calculation logic

--- game_of_life.py
+++ game_of_life.py
@@ -13,14 +13,14 @@ from matplotlib import pyplot as plt
 import random as r
 
 # System variables
-width = 60
-height = 60
+width = 5
+height = 5
 culture = array([[0 for i in range(width)] for j in range(height)])
 
 life_of_culture = 10
 age_of_culture = 0
 
-no_of_seeds = 1800
+no_of_seeds = 16
 
 seeds = []
 for _ in range(no_of_seeds):
@@ -35,8 +35,8 @@ def simulate():
                 neighbours = [culture[_ - 1, __ - 1], culture[_ - 0, __ - 1], culture[_ + 1, __ - 1],
                               culture[_ - 1, __ - 0], culture[_ + 1, __ - 0], culture[_ - 1, __ + 1],
                               culture[_ + 1, __ - 0], culture[_ + 1, __ + 1]]
-                immediate_neighbours = [culture[_, __ - 1], culture[_ - 1, __],
-                                        culture[_ + 1, __], culture[_, __ + 1]]
+                immediate_neighbours = [culture[_ - 1, __], culture[_, __ - 1],
+                                        culture[_, __ + 1], culture[_ + 1, __]]
                 no_of_alive_neighbours = sum(immediate_neighbours)
             except IndexError as ie:
                 pass
@@ -48,11 +48,11 @@ def simulate():
             Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
             '''
 
-            if no_of_alive_neighbours < 2 and culture[_, __] == 1:
+            if no_of_alive_neighbours < 2:
                 next_gen[_, __] = 0
             elif no_of_alive_neighbours > 3:
                 next_gen[_, __] = 0
-            elif (no_of_alive_neighbours == 3) and culture[_, __] == 0:
+            elif (no_of_alive_neighbours == 3):
                 next_gen[_, __] = 1
     return next_gen
 
@@ -62,15 +62,42 @@ def plant_seeds(seeds, culture):
         culture[seed[0], seed[1]] = 1
     return culture
 
+def members():
+    next_gen = culture
+    for _ in range(width):
+        for __ in range(height):
+             try:
+                 u = culture[_ - 1, __]
+             except IndexError as ie:
+                 u = 0
+             try:
+                 l = culture[_, __ - 1]
+             except IndexError as ie:
+                 l = 0
+             try:
+                 r = culture[_, __+ 1]
+             except IndexError as ie:
+                 r = 0
+             try:
+                 d = culture[_ + 1, __]
+             except IndexError as ie:
+                 d = 0
+
+             immediate_neighbours = [u, l, r, d]
+             no_of_alive_neighbours = sum(immediate_neighbours)
+             print _,__,culture[_,__], immediate_neighbours
+
 
 def print_culture(culture):
     plt.figure(age_of_culture)
     plt.imshow(culture, interpolation = 'nearest')
     plt.show()
+    # print culture
 
 
 if __name__ == '__main__':
     culture = plant_seeds(seeds, culture)
+    members()
     print_culture(culture)
 
     while age_of_culture != life_of_culture:
