a40cb3beeffe92355967c25f9ebdaaeac023d80d
piyushagade <piyushagade@gmail.com>
2016-10-29 01:28:51 -0400

    Logic corrected. Three shapes added.

--- assets/kanyes_game_of_life.py
+++ assets/kanyes_game_of_life.py
@@ -3,7 +3,7 @@ Author: Piyush Agade
 
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-                        Conway's Game of Life
+                        Kanye's Game of Life
 
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 '''
@@ -11,7 +11,10 @@ import os
 import random as r
 from sys import stdout
 
+from PIL import Image
 from matplotlib import pyplot as plt
+from matplotlib._png import read_png
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage
 from numpy import array
 import imageio
 
@@ -20,106 +23,60 @@ import imageio
 # ***********************************************************************
 
 # Culture dimensions
-width = 50
-height = 50
+width = 15
+height = 15
 
 # Culture behavioral parmaters
-life_of_culture = 20
+life_of_culture = 10
 age_of_culture = 0
 
-no_of_seeds = 8868
+no_of_seeds = 168
 
 # GIF parameters
 fps = 1     # Higher the fs, faster the gif animation will be.
-
-# First generation
-
-# 1. Glider
-x = 5
-y = 0
-copies = 1
-
-# 2. Mini Bomb
-
-# 3. Bomb
-
-shape = 'Glider'
 
 
 # **********************************************************************
 #                       Nothing to be changed below
 # **********************************************************************
 
-temp_x = x
-temp_y = y
-if shape.lower().replace(" ", "") == 'glider':
-    for _ in range(copies):
-        seeds = ((2+y,1+x),(3+y,2+x),(3+y,3+x),(2+y,3+x),(1+y,3+x))
-        x += temp_x
-        y += temp_y
-elif shape.lower().replace(" ", "") == 'minibomb':
-    y = width/2
-    x = height/2
-    seeds = ((1+y,2+x),(2+y,1+x),(2+y,2+x),(2+y,3+x),(3+y,1+x),(3+y,3+x),(4+y,2+x))
-elif shape.lower().replace(" ", "") == 'bomb':
-    y = width/2
-    x = height/2
-    seeds = ((1+y,1+x),(2+y,1+x),(3+y,1+x),(4+y,1+x),(5+y,1+x),(1+y,3+x),(5+y,3+x), (1+y,5+x),(2+y,5+x),(3+y,5+x),(4+y,5+x),(5+y,5+x))
-
-else:
-    print "Wrong option selected."
-
-
 culture = array([[0 for i in range(width)] for j in range(height)])
-#
-# for _ in range(no_of_seeds):
-#     seeds.append((r.randint(0, height - 1), r.randint(0, width - 1)))
+
+seeds = []
+
+for _ in range(no_of_seeds):
+    seeds.append((r.randint(0, height - 1), r.randint(0, width - 1)))
 
 population = 0
 
+kim = read_png('assets/kim.png')
+imagebox = OffsetImage(kim, zoom=.1)
+kim_pos = [0,0]
 
 def simulate():
-    next_gen = array([[0 for i in range(width)] for j in range(height)])
+    next_gen = culture
     for _ in range(width):
         for __ in range(height):
-
             if _ == 0 or __ == 0:
                 if _ == 0:
                     u = 0
                 if __ == 0:
                     l = 0
             else:
-                u = culture[_ - 1, __]
-                l = culture[_, __ - 1]
+                u = next_gen[_ - 1, __]
+                l = next_gen[_, __ - 1]
             try:
-                r = culture[_, __ + 1]
+                r = next_gen[_, __ + 1]
             except IndexError:
                 r = 0
             try:
-                d = culture[_ + 1, __]
+                d = next_gen[_ + 1, __]
             except IndexError:
                 d = 0
-            try:
-                ur = culture[_ - 1, __ + 1]
-            except IndexError:
-                ur = 0
-            try:
-                dr = culture[_ + 1, __ + 1]
-            except IndexError:
-                dr = 0
-            try:
-                dl = culture[_ + 1, __ - 1]
-            except IndexError:
-                dl = 0
-            try:
-                ul = culture[_ - 1, __ - 1]
-            except IndexError:
-                ul = 0
 
             immediate_neighbours = [u, l, r, d]
-            neighbours = [ul, u, ur, l, r, dl, d, dr]
-            no_of_alive_neighbours = sum(neighbours)
-
+            no_of_alive_neighbours = sum(immediate_neighbours)
+            # print _,__,next_gen[_,__], immediate_neighbours
 
 
             '''
@@ -129,46 +86,26 @@ def simulate():
             Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
             '''
 
-            if no_of_alive_neighbours < 2 and culture[_,__] == 1:
+            if no_of_alive_neighbours < 2:
                 next_gen[_, __] = 0
-            elif no_of_alive_neighbours in (2,3) and culture[_,__] == 1:
-                next_gen[_, __] = 1
-            elif no_of_alive_neighbours > 3 and culture[_,__] == 1:
+            elif no_of_alive_neighbours > 3:
                 next_gen[_, __] = 0
-            elif (no_of_alive_neighbours == 3) and culture[_,__] == 0:
+            elif (no_of_alive_neighbours == 3):
                 next_gen[_, __] = 1
-
-
-            # Condensed if-else
-            # if culture[_,__] == 1:
-            #     if no_of_alive_neighbours not in (2,3):
-            #         next_gen[_,__] = 0
-            #     else:
-            #         next_gen[_,__] = 1
-            # else:
-            #     if no_of_alive_neighbours == 3:
-            #         next_gen[_,__] = 1
-
-
     return next_gen
 
 
 def plant_seeds(seeds, culture):
     for seed in seeds:
-        try:
-            culture[seed[0], seed[1]] = 1
-        except IndexError:
-            pass
+        culture[seed[0], seed[1]] = 1
     return culture
 
 
 def print_culture(culture):
-    plt.figure(age_of_culture)
+    fig = plt.figure()
     plt.imshow(culture, interpolation = 'nearest')
-    plt.title(
-        'Generation: ' + str(age_of_culture) + '/' + str(life_of_culture) + '\nPopulation: ' + str(sum(sum(culture))))
+    plt.title('Generation: ' + str(age_of_culture) + '/' + str(life_of_culture) + '\nPopulation: ' + str(sum(sum(culture))))
     plt.savefig('png/' + str(age_of_culture))
-    # plt.show()
     writeGIF()
 
 
@@ -181,21 +118,41 @@ def writeGIF():
 
     imageio.mimsave('./gif/animation.gif', readImages, fps = fps)
 
+def putKim(culture):
+    for _ in range(width):
+        for __ in range(height):
+            if culture[_,__] == 1:
+                fig = plt.gcf()
+                fig.clf()
+                ax = plt.subplot(111)
+
+                # add a first image
+                imagebox = OffsetImage(kim, zoom=.1)
+                xy = [0.25, 0.45]               # coordinates to position this image
+
+                ab = AnnotationBbox(imagebox, xy,
+                    xybox=(30., -30.),
+                    xycoords='data',
+                    boxcoords="offset points")
+                ax.add_artist(ab)
+
 
 if __name__ == '__main__':
-    #simulate the alphas
+    # simulate the alphas
     culture = plant_seeds(seeds, culture)
     print_culture(culture)
 
+
     # simulate the descendants
     while age_of_culture != life_of_culture:
-
         culture = simulate()
         age_of_culture += 1
         print_culture(culture)
 
 
-
+    # putKim(culture)
 
     # announce completion of the program
     stdout.write("\nGIF generated in ./gif.")
+
+
